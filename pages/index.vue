16508cacf57a15d3248bf08104efdc2100e00a4c
akramjonnuriddinov <nn.akramjon@gmail.com>
2024-12-19 15:03:39 +0500

    add seo for index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,20 @@
+<script setup lang="ts">
+useSeoMeta({
+  title: "Akramjon Nuriddinov | Vue.js developer",
+  ogTitle: "Akramjon Nuriddinov | Vue.js developer",
+  description: "About Me | Frontend Developer with Vue.js and Nuxt Expertise",
+  ogDescription: "About Me | Frontend Developer with Vue.js and Nuxt Expertise",
+  ogImage: "https://example.com/image.png",
+  twitterCard: "summary_large_image",
+  keywords:
+    "Vue.js developer, Nuxt.js developer, frontend developer, Tailwind CSS expert, web developer portfolio",
+})
+</script>
+
 <template>
   <AppHero />
   <LatestPost />
   <AppCategories />
   <NuxtRouteAnnouncer />
-  <NuxtWelcome />
+  <!-- <NuxtWelcome /> -->
 </template>
