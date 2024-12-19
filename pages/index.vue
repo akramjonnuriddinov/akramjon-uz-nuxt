4209011bdded6554de34f93f84a13beb0dc3c475
akramjonnuriddinov <nn.akramjon@gmail.com>
2024-12-19 15:04:43 +0500

    change image

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,8 @@ useSeoMeta({
   ogTitle: "Akramjon Nuriddinov | Vue.js developer",
   description: "About Me | Frontend Developer with Vue.js and Nuxt Expertise",
   ogDescription: "About Me | Frontend Developer with Vue.js and Nuxt Expertise",
-  ogImage: "https://example.com/image.png",
+  ogImage:
+    "https://pbs.twimg.com/profile_images/1696835964881002498/0YEYRNGF_400x400.png",
   twitterCard: "summary_large_image",
   keywords:
     "Vue.js developer, Nuxt.js developer, frontend developer, Tailwind CSS expert, web developer portfolio",
