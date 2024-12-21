beef8d78ad96a447366b7be3f2912aef232c60b3
akramjonnuriddinov <nn.akramjon@gmail.com>
2024-12-21 14:07:38 +0500

    change favicon icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,7 +15,7 @@ export default defineNuxtConfig({
     head: {
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
-      link: [{ rel: 'icon', type: 'image/x-icon', href: '/n_icon.png' }]
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/fav.png' }]
     }
   },
 
