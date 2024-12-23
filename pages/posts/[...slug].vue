7e6d98da3661ca4bb3eea531896af8969c2b6120
akramjonnuriddinov <nn.akramjon@gmail.com>
2024-12-23 09:44:35 +0500

    add next tick for copy button

--- pages/posts/[...slug].vue
+++ pages/posts/[...slug].vue
@@ -4,17 +4,19 @@ definePageMeta({
 })
 
 onMounted(() => {
-  document.querySelectorAll("pre").forEach((block) => {
-    const button = document.createElement("button")
-    button.innerText = "Copy"
-    button.className = "copy-btn"
-    block.appendChild(button)
+  nextTick(() => {
+    document.querySelectorAll("pre").forEach((block) => {
+      const button = document.createElement("button")
+      button.innerText = "Copy"
+      button.className = "copy-btn"
+      block.appendChild(button)
 
-    button.addEventListener("click", () => {
-      const code = block.querySelector("code")?.innerText || ""
-      navigator.clipboard.writeText(code).then(() => {
-        button.innerText = "Copied!"
-        setTimeout(() => (button.innerText = "Copy"), 2000)
+      button.addEventListener("click", () => {
+        const code = block.querySelector("code")?.innerText || ""
+        navigator.clipboard.writeText(code).then(() => {
+          button.innerText = "Copied!"
+          setTimeout(() => (button.innerText = "Copy"), 2000)
+        })
       })
     })
   })
