c42b8da4c31f71863e1a3836d1a8376384d95860
akramjonnuriddinov <nn.akramjon@gmail.com>
2024-12-23 11:03:37 +0500

    add seo to nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,9 +13,16 @@ export default defineNuxtConfig({
 
   app: {
     head: {
+      title: "Akramjon Nuriddinov | Vue.js Developer",
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
-      link: [{ rel: 'icon', type: 'image/x-icon', href: '/fav.png' }]
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/fav.png' }],
+      meta: [
+        { hid: 'description', name: 'description', content: 'Blog website from Vue.js developer' },
+        { hid: 'og:description', property: 'og:description', content: 'Blog website from Vue.js developer' },
+        { hid: 'og:title', property: 'og:title', content: 'Akramjon.N | Vue.js developer' },
+        { hid: 'og:keywords', property: 'og:keywords', content: 'Akramjon.N | Vue.js developer' },
+      ]
     }
   },
 
