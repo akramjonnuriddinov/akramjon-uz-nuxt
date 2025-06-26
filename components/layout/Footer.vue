470664c64ee7de451cd9395e0928059d304eb566
akramjonnuriddinov <nn.akramjon@gmail.com>
2025-06-26 19:30:58 +0500

    fix footer mobile design

--- components/layout/Footer.vue
+++ components/layout/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="py-10">
     <LayoutContainer>
-      <div class="flex items-center justify-between">
+      <div class="flex flex-col gap-4 md:flex-row items-center justify-between">
         <p class="whitespace-nowrap">
           Copyright © 2025 | {{ $t("copyright") }}
         </p>
